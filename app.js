a8c1e2222e7b2d7c3f5be896f09961fcb3193197
dominicdaviescodes <codewithdom@gmail.com>
2020-08-19 20:36:56 +0100

    js, code out div which will contain the list, add the check and trash buttons

--- app.js
+++ app.js
@@ -4,6 +4,31 @@ const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
 // event listeners
-
+todoButton.addEventListener('click', addTodo);
 
 // functions
+
+function addTodo(event) {
+  // prevent form from submitting
+  event.preventDefault();
+  // Todo div
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  // create li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = "example";
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  // check button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>'
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+  // trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+  trashButton.classList.add('complete-btn');
+  todoDiv.appendChild(trashButton);
+  // append to list
+  todoList.appendChild(todoDiv);
+}
