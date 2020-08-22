d18c261310568b322eca7e4f7599732a87d95e2c
dominicdaviescodes <codewithdom@gmail.com>
2020-08-22 12:28:11 +0100

    js, when you enter a todo it gets added to list

--- app.js
+++ app.js
@@ -8,6 +8,32 @@ const todoList = document.querySelector('.todo-list')
 
 // event listeners
 
+todoButton.addEventListener('click', addTodo);
 
 
 // functions
+
+function addTodo(event) {
+  // prevent form from submitting
+  event.preventDefault;
+  // put each todo item in the todo div
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+  // create li
+  const newTodo = document.createElement('li');
+  newTodo.innerText = 'test';
+  newTodo.classList.add('todo-item');
+  todoDiv.appendChild(newTodo);
+  // check off button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn')
+  todoDiv.appendChild(completedButton);
+  // delete button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('complete-btn')
+  todoDiv.appendChild(trashButton);
+  // Append to list
+  todoList.appendChild(todoDiv);
+}
